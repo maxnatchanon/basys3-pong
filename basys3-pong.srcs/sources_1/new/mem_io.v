8e514e7b6019724e333b8e7382be62e575267919
Natchanon A <natchanon.347@gmail.com>
2019-04-24 15:00:24 +0700

    Add start game setup

--- basys3-pong.srcs/sources_1/new/mem_io.v
+++ basys3-pong.srcs/sources_1/new/mem_io.v
@@ -9,9 +9,9 @@ module mem_io(
     output wire [6:0] seg,      // 7-Seg number display
     output wire dp,             // 7-Seg dot display
     output wire [3:0] an,       // 7-Seg selector
-    output wire [11:0] rgb,         // VGA color
-    output wire hsync,              // H-sync signal
-    output wire vsync,              // V-sync sugnal
+    output wire [11:0] rgb,     // VGA color
+    output wire hsync,          // H-sync signal
+    output wire vsync,          // V-sync sugnal
     inout wire [7:0] data,      // Input-output data
     input wire [9:10] address,  // Input address
     input wire wr,              // Write signal (Active high)
@@ -27,9 +27,8 @@ module mem_io(
     // Keycode                  => 3FE - 3FF
     // Game state               => 000
     // Player position 1/2      => 001 - 002
-    // Player movement 1/2      => 003 - 004
     // Ball position x/y        => 011 - 012
-    // Ball speed x/y           => 101 - 102
+    // Ball speed x/y           => 101 - 102    (1 - to left/up, 2 - to right/down)
     // Score 1/2                => 103 - 104
 
     reg [DATA_WIDTH-1:0] mem [(1<<ADDRESS_WIDTH)-1:0];
@@ -39,7 +38,7 @@ module mem_io(
     assign data = (wr == 0) ? data_out : 10'bz;
 
     // 7-segment display
-    seven_segment SEVEN_SEG(seg,an,dp,keycode,clk);
+    seven_segment SEVEN_SEG(seg,an,dp,{mem[10'h3fe,mem[10'h3ff]]},clk);
 
     // Keyboard
     keyboard KB(keycode,ps2_data,ps2_clk,clk,nreset);
@@ -65,11 +64,21 @@ module mem_io(
     begin
         {mem[16'h3FE],mem[16'h3FF]} <= keycode;
         case (keycode)
-        16'h001C: if (mem[10'h001] > 0)  mem[10'h001] <= mem[10'h001] - 1;
-        16'h001B: if (mem[10'h001] < 19) mem[10'h001] <= mem[10'h001] + 1;
-        16'h0042: if (mem[10'h002] > 0)  mem[10'h002] <= mem[10'h002] - 1;
-        16'h004B: if (mem[10'h002] < 19) mem[10'h002] <= mem[10'h002] + 1;
-        16'h0029: if (mem[10'h000] == 0) mem[10'h000] <= 1;
+        16'h001C: if (mem[10'h001] > 0  & mem[10'h000] == 1) mem[10'h001] <= mem[10'h001] - 1;  // A
+        16'h001B: if (mem[10'h001] < 19 & mem[10'h000] == 1) mem[10'h001] <= mem[10'h001] + 1;  // S
+        16'h0042: if (mem[10'h002] > 0  & mem[10'h000] == 1) mem[10'h002] <= mem[10'h002] - 1;  // K
+        16'h004B: if (mem[10'h002] < 19 & mem[10'h000] == 1) mem[10'h002] <= mem[10'h002] + 1;  // L
+        16'h0029:                                                                               // SPACE
+            if (mem[10'h000] == 0)
+            begin 
+                mem[10'h000] <= 1;      // Set game state to 1
+                mem[10'h001] <= 10;     // Set paddle 1 position
+                mem[10'h002] <= 10;     // Set paddle 2 position
+                mem[10'h011] <= 40;     // Set ball x position
+                mem[10'h012] <= 12;     // Set ball y position
+                mem[10'h101] <= 1;      // Set ball x speed
+                mem[10'h102] <= 1;      // Set ball y speed
+            end
         endcase
     end
 
