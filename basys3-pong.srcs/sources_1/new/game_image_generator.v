9fa64b77a8791acc8e51cb1068b28c07242aa451
Natchanon A <natchanon.347@gmail.com>
2019-04-26 12:48:36 +0700

    Fix score and game template

--- basys3-pong.srcs/sources_1/new/game_image_generator.v
+++ basys3-pong.srcs/sources_1/new/game_image_generator.v
@@ -19,7 +19,7 @@ module game_image_generator(
     input wire [8:0] y
     );
 
-    // Score position - row:[44:56] col:[16:24]&[55:63]
+    // Score position - row:[44:56] col:[17:23]&[56:62]
     // Game position - row:[15:39]
     // Player position - col:[3]&[79]
 
@@ -34,7 +34,23 @@ module game_image_generator(
 
     initial
     begin
-        $readmemb("gameTemplate.mem",game_area);
+        // $readmemb("gameTemplate.mem",game_area);
+        game_area[3][54:25]  = 25'b111111001111110010000100111111;
+        game_area[4][54:25]  = 25'b100001001000010011000100100000;
+        game_area[5][54:25]  = 25'b100001001000010011000100100000;
+        game_area[6][54:25]  = 25'b100001001000010010100100100000;
+        game_area[7][54:25]  = 25'b111111001000010010010100100111;
+        game_area[8][54:25]  = 25'b100000001000010010010100100001;
+        game_area[9][54:25]  = 25'b100000001000010010001100100001;
+        game_area[10][54:25] = 25'b100000001000010010001100100001;
+        game_area[11][54:25] = 25'b100000001111110010000100111111;
+        game_area[14] = 80'hffffffffffffffffffff;
+        game_area[40] = 80'hffffffffffffffffffff;
+        game_area[19][40:39] = 2'b11;
+        game_area[23][40:39] = 2'b11;
+        game_area[27][40:39] = 2'b11;
+        game_area[31][40:39] = 2'b11;
+        game_area[35][40:39] = 2'b11;
     end
 
     number_generator NUM1(score_pixel_1,score_1);
@@ -87,37 +103,37 @@ module game_image_generator(
         if (score_1 != c_score_1)
         begin
             c_score_1 = score_1;
-            game_area[44][63:55] <= score_pixel_1[8:0];
-            game_area[45][63:55] <= score_pixel_1[17:9];
-            game_area[46][63:55] <= score_pixel_1[26:18];
-            game_area[47][63:55] <= score_pixel_1[35:27];
-            game_area[48][63:55] <= score_pixel_1[44:36];
-            game_area[49][63:55] <= score_pixel_1[53:45];
-            game_area[50][63:55] <= score_pixel_1[62:54];
-            game_area[51][63:55] <= score_pixel_1[71:63];
-            game_area[52][63:55] <= score_pixel_1[80:72];
-            game_area[53][63:55] <= score_pixel_1[89:81];
-            game_area[54][63:55] <= score_pixel_1[98:90];
-            game_area[55][63:55] <= score_pixel_1[107:99];
-            game_area[56][63:55] <= score_pixel_1[116:108];
+            game_area[44][62:56] <= score_pixel_1[6:0];
+            game_area[45][62:56] <= score_pixel_1[13:7];
+            game_area[46][62:56] <= score_pixel_1[20:14];
+            game_area[47][62:56] <= score_pixel_1[27:21];
+            game_area[48][62:56] <= score_pixel_1[34:28];
+            game_area[49][62:56] <= score_pixel_1[41:35];
+            game_area[50][62:56] <= score_pixel_1[48:42];
+            game_area[51][62:56] <= score_pixel_1[55:49];
+            game_area[52][62:56] <= score_pixel_1[62:56];
+            game_area[53][62:56] <= score_pixel_1[69:63];
+            game_area[54][62:56] <= score_pixel_1[76:70];
+            game_area[55][62:56] <= score_pixel_1[83:77];
+            game_area[56][62:56] <= score_pixel_1[90:84];
         end
 
         if (score_2 != c_score_2)
         begin
             c_score_2 = score_1;
-            game_area[44][24:16] <= score_pixel_2[8:0];
-            game_area[45][24:16] <= score_pixel_2[17:9];
-            game_area[46][24:16] <= score_pixel_2[26:18];
-            game_area[47][24:16] <= score_pixel_2[35:27];
-            game_area[48][24:16] <= score_pixel_2[44:36];
-            game_area[49][24:16] <= score_pixel_2[53:45];
-            game_area[50][24:16] <= score_pixel_2[62:54];
-            game_area[51][24:16] <= score_pixel_2[71:63];
-            game_area[52][24:16] <= score_pixel_2[80:72];
-            game_area[53][24:16] <= score_pixel_2[89:81];
-            game_area[54][24:16] <= score_pixel_2[98:90];
-            game_area[55][24:16] <= score_pixel_2[107:99];
-            game_area[56][24:16] <= score_pixel_2[116:108];
+            game_area[44][23:17] <= score_pixel_2[6:0];
+            game_area[45][23:17] <= score_pixel_2[13:7];
+            game_area[46][23:17] <= score_pixel_2[20:14];
+            game_area[47][23:17] <= score_pixel_2[27:21];
+            game_area[48][23:17] <= score_pixel_2[34:28];
+            game_area[49][23:17] <= score_pixel_2[41:35];
+            game_area[50][23:17] <= score_pixel_2[48:42];
+            game_area[51][23:17] <= score_pixel_2[55:49];
+            game_area[52][23:17] <= score_pixel_2[62:56];
+            game_area[53][23:17] <= score_pixel_2[69:63];
+            game_area[54][23:17] <= score_pixel_2[76:70];
+            game_area[55][23:17] <= score_pixel_2[83:77];
+            game_area[56][23:17] <= score_pixel_2[90:84];
         end
 
     end
